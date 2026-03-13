a0b1865a39e673b5cfbdb2e1a6a65b6397d16d24
Kolter Saurey <0396@asirobots.com>
2026-03-13 14:01:57 -0600

    fix: Remove email field from user registration form

--- users/forms.py
+++ users/forms.py
@@ -5,4 +5,4 @@ from .models import User
 class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'password1', 'password2')

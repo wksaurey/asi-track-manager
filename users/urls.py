6b50bfd64af174f427d635a2e255223d2b923452
Hollis B <blueartlover6@gmail.com>
2026-03-19 11:44:12 -0600

    feat: Add user management page for admin user promotion

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
+    path('management/', views.user_management, name='user_management'),
+    path('toggle-admin/<int:user_id>/', views.toggle_admin, name='toggle_admin'),
+    path('delete-user/<int:user_id>/', views.delete_user, name='delete_user'),
 ]
